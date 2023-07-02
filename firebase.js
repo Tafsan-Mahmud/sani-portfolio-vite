0c367f76c7f5020df9c7786927f8d56c0d3d0b7d
Tafsan-Mahmud <tafsanmahmudnobin090@gmail.com>
2023-07-02 21:21:12 +0600

    complete new deploy and hosting

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCild9LE25p86U6Uo5supeEO5iQadL1fz4",
+  authDomain: "freelancer-sani.firebaseapp.com",
+  projectId: "freelancer-sani",
+  storageBucket: "freelancer-sani.appspot.com",
+  messagingSenderId: "1035856221743",
+  appId: "1:1035856221743:web:13221c9e04b6cea0bcd8da"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
